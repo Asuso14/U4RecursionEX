af536035dd83ffc3d3190fee5d897075aa470b2b
NASHELAPTOP\Usuario <nachogamallolafon123@gmail.com>
2025-01-13 21:15:23 +0100

    Ejercicios Programación

--- U4BateryPath/src/main/java/org/example/Exercises.java
+++ U4BateryPath/src/main/java/org/example/Exercises.java
@@ -10,4 +10,43 @@ public class Exercises {
             investedNum(num/10);
         }
     }
+
+    public static void investChain(int count, char [] UserChain){
+        if (count == 0){
+            System.out.print(UserChain[count]);
+        }else {
+            System.out.print(UserChain[count]);
+            investChain(--count,UserChain);
+        }
+    }
+
+    public static boolean AlphabeticalSort(int count, char [] UserChain){
+        if (count<UserChain.length - 1){
+            if(UserChain[count] <= UserChain[count+1]){
+                return AlphabeticalSort(++count,UserChain);
+            }else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean CheckBinaryValue(int num){
+        if (num >= 9){
+            if ((num % 10) == 0 || (num % 10) == 1){
+                return CheckBinaryValue(num/10);
+            }else {
+                return false;
+            }
+        }
+        return num == 0 || num == 1;
+    }
+
+    public static String ConvertToBinary (int num){
+        if(num == 0 || num == 1) {
+            return Integer.toString(num);
+        }else {
+            return ConvertToBinary(num / 2) + num % 2;
+        }
+    }
 }
